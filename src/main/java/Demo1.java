8de9906c9fdbbd8bde7ebbb7c46a37298833a4cb
suraj17ee <suraj17ee@gmail.com>
2024-09-21 00:47:55 +0530

    string task1 added

--- src/main/java/Demo1.java
+++ src/main/java/Demo1.java
@@ -1,7 +1,9 @@
-package main.java;
-
 public class Demo1 {
     public static void main(String[] args) {
-        System.out.print("git practice begins here");
+        System.out.println("git practice begins here");
+        String str = "java is a programming language";
+        for (String word : str.split(" ")) {
+            System.out.println(word + " - lenth of this word is: " + word.length());
+        }
     }
 }
