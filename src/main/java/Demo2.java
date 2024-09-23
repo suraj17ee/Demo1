3563dc2927f4a23064df54696f33db6adb9de614
suraj17ee <suraj17ee@gmail.com>
2024-09-23 13:21:45 +0530

    Demo2 added

---
+++ src/main/java/Demo2.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+class Car {
+    private String brandName;
+    private String carModel;
+    private String carEngineType;
+
+    public Car(String brandName, String carModel, String carEngineType) {
+        this.brandName = brandName;
+        this.carModel = carModel;
+        this.carEngineType = carEngineType;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getCarModel() {
+        return carModel;
+    }
+
+    public void setCarModel(String carModel) {
+        this.carModel = carModel;
+    }
+
+    public String getCarEngineType() {
+        return carEngineType;
+    }
+
+    public void setCarEngineType(String carEngineType) {
+        this.carEngineType = carEngineType;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brandName='" + brandName + '\'' +
+                ", carModel='" + carModel + '\'' +
+                ", carEngineType='" + carEngineType + '\'' +
+                '}';
+    }
+}
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Car car1 = new Car("TATA", "Punch", "petrol");
+        Car car2 = new Car("HUNDAI", "Dexter", "petrol");
+        Car car3 = new Car("HONDA", "Accord", "diesel");
+        Car car4 = new Car("TATA", "Herier", "petrol");
+        Car car5 = new Car("TATA", "Curvv", "diesel");
+        List<Car> cars = List.of(car1, car2, car3, car4, car5);
+        cars.stream().filter(c -> Objects.equals(c.getCarEngineType(), "diesel"))
+                .forEach(car -> System.out.println(car.getCarModel()));
+    }
+}
